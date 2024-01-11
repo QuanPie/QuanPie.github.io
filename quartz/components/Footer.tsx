8739033ec6d7871e3d99544dd1dd9c9c281f8492
cream314159 <ss1041310413@gmail.com>
2024-01-11 22:27:29 +0800

    footer edit - cc license edit

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -37,15 +37,15 @@ export default ((opts?: Options) => {
           <p>
               This work by 
               <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="http://mailto:quanpie2000@gmail.com">
-                Quan_Pie
+                &nbsp;Quan_Pie
               </a>
               &nbsp;is licensed under
               <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">
                 &nbsp;CC BY-NC-SA 4.0
-                <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"/>
+                {/* <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"/>
                 <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"/>
                 <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"/>
-                <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"/>
+                <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"/> */}
               </a>
           </p>
         </ul>
